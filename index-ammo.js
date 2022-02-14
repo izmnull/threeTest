e55a4793ef5f859ac7e2a42861709ebabce5ec6d
izmnull <98688029+izmnull@users.noreply.github.com>
2022-02-15 01:12:41 +0900

    complete

--- index-ammo.js
+++ index-ammo.js
@@ -5,11 +5,12 @@ import * as THREE from 'three';
 import Stats from 'Stats';
 // import GUI from 'GUI';
 import {OrbitControls} from 'OrbitControls';
-// import {TransformControls} from 'TransformControls';
+import {TransformControls} from 'TransformControls';
 import {XHRLoader, PointLight, SpotLight} from 'three';
 
 class APP {
     constructor() {
+        this.clock = new THREE.Clock();
         this.physicsWorld = null;
         this.transformAux = null;
         this.dynamicObjects = [];
@@ -25,20 +26,24 @@ class APP {
             this.scene.add(new THREE.AxesHelper(100));
         };
         this.addControls = function () {
-            const controls = new OrbitControls(this.camera, this.renderer.domElement);
+            this.orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
+            this.transformControls = new TransformControls(this.camera, this.renderer.domElement);
+            this.orbitControls.update();
 
-            controls.update();
+            this.scene.add(this.transformControls);
         };
         this.addLights = function () {
+            this.scene.add(new THREE.AmbientLight(0x111111));
+
             const lights = [
-                new PointLight(0xffffff, 0.5, 0),
-                new PointLight(0xffffff, 0.5, 0),
-                new PointLight(0xffffff, 0.5, 0)
+                new PointLight(0xffffff, 0.47, 0),
+                new PointLight(0xffffff, 0.47, 0),
+                new PointLight(0xffffff, 0.47, 0)
             ];
 
             lights[0].position.set(0, 200, 0);
             lights[1].position.set(100, 200, 100);
-            lights[2].position.set(-100, -200, -100);
+            lights[2].position.set(0, 0, 500);
 
             // lights[0].castShadow = true;
             // lights[0].shadow.mapSize.width = 2048;
@@ -46,6 +51,12 @@ class APP {
             lights.forEach((light) => {
                 this.scene.add(light);
             });
+
+            const spotLight = new THREE.SpotLight(0xffffff, 0.2, 0, Math.PI/5, 20);
+
+            spotLight.position.set(0, 20, 200);
+            this.scene.add(spotLight);
+            this.transformControls.attach(spotLight);
         };
         this.addEvent = function () {
             window.addEventListener('resize', () => {
@@ -54,6 +65,24 @@ class APP {
 
                 this.renderer.setSize(window.innerWidth, window.innerHeight);
             });
+
+            this.transformControls.addEventListener('dragging-changed', (event) => {
+                this.orbitControls.enabled = !event.value;
+            });
+
+            // window.addEventListener('keydown', (event) => {
+            //     switch (event.key) {
+            //         case 'g':
+            //             this.transformControls.setMode('translate');
+            //             break
+            //         case 'r':
+            //             this.transformControls.setMode('rotate');
+            //             break
+            //         case 's':
+            //             this.transformControls.setMode('scale');
+            //             break;
+            //     }
+            // });
         };
         this.setupThree = function () {
             this.scene = new THREE.Scene();
@@ -282,14 +311,30 @@ class APP {
 
             return {arg, objectBody};
         };
+        this.texture = (function () {
+            return {
+                medalFace: new THREE.TextureLoader().load('textures/face.jpg'),
+                medalSide: new THREE.TextureLoader().load('textures/side.jpg'),
+                medalFaceBump: new THREE.TextureLoader().load('textures/face_bump.jpg'),
+                medalSideBump: new THREE.TextureLoader().load('textures/side_bump.jpg')
+            };
+        }());
+        this.phongMaterial = function () {
+            return new THREE.MeshPhongMaterial({
+                color: 0x666666,
+                emissive: 0x333333,
+                specular: 0x999999,
+                shininess: 10
+            });
+        };
         this.appendObject = {
             ground: function () {
-                let size = new Ammo.btVector3(20, 5, 15);
+                let size = new Ammo.btVector3(20, 2, 22.5);
 
                 return {
                     mass: 0,
                     margin: 0,
-                    pos: new Ammo.btVector3(0, -5, 9.5),
+                    pos: new Ammo.btVector3(0, -2, 2.5),
                     // rot: {x: 0, y: 45, z: 0},
                     threeObject: (function () {
                         return new THREE.Mesh(
@@ -308,26 +353,25 @@ class APP {
                     localInertia: new Ammo.btVector3(0, 0, 0),
                     afterPhy: (body) => {
                         body.setRestitution(1); // 反発係数
-                        body.setFriction(1); // 摩擦係数
+                        body.setFriction(1);    // 摩擦係数
                         body.setAngularFactor(new Ammo.btVector3(0, 0, 0)); // 回転制限
                         body.setLinearFactor(new Ammo.btVector3(0, 0, 0)); // 滑り制限
                     },
-                    updatePhy: function (body, p, q) {
-                        // console.log(this.flag);
+                    updatePhy: function (body) {
                         // body.setAngularVelocity(new Ammo.btVector3(0, +2, 0));
                     }
                 };
             },
             upperGround: function () {
-                let size = new Ammo.btVector3(20, 3, 10);
+                let size = new Ammo.btVector3(20, 3, 12.5);
                 let flag = true;
 
                 return {
                     mass: 10,
                     margin: 0,
-                    pos: new Ammo.btVector3(0, 3, -10),
+                    pos: new Ammo.btVector3(0, 3, -12.5),
                     // rot: {x: 0, y: 45, z: 0},
-                    threeObject: (function () {
+                    threeObject: (() => {
                         return new THREE.Mesh(
                             new THREE.BoxGeometry(
                                 size.x() * 2,
@@ -335,49 +379,49 @@ class APP {
                                 size.z() * 2
                             ),
                             new THREE.MeshPhongMaterial({
-                                color: 0x66ffbb,
-                                specular: 0x222222
+                                color: 0x464759,
+                                emissive: 0x333336,
+                                specular: 0xcccccc,
+                                shininess: 10,
+                                map: this.texture.medalSide
                             })
                         );
-                    }()),
+                    })(),
                     collisionShape: new Ammo.btBoxShape(size),
                     localInertia: new Ammo.btVector3(0, 0, 0),
                     afterPhy: (body) => {
-                        // return;
                         body.setRestitution(5); // 反発係数
                         body.setFriction(10); // 摩擦係数
                         body.setAngularFactor(new Ammo.btVector3(0, 0, 0)); // 回転制限
                         body.setLinearFactor(new Ammo.btVector3(0, 0, 0)); // 滑り制限
                     },
                     updatePhy: function (body, p, q, objThree, i) {
-                        let c = -5;
+                        let val = -5;
 
                         if (flag) {
-                            c = 5;
+                            val = 5;
 
-                            if (p.z() > 0) {
+                            if (p.z() >= -10.5) {
                                 flag = false;
-                                c = -5;
+                                val = -5;
                             }
                         } else {
-                            if (p.z() <= -10) {
+                            if (p.z() <= -20) {
                                 flag = true;
                             }
                         }
 
-                        body.setLinearVelocity(new Ammo.btVector3(0, 0, +c));
+                        body.setLinearVelocity(new Ammo.btVector3(0, 0, +val));
                     }.bind(this)
                 };
             }.bind(this),
-            wallLeft: function () { },
-            wallRight: function () { },
-            wallBack: function () {
-                let size = new Ammo.btVector3(20, 10, 1);
+            wallLeft: function () {
+                let size = new Ammo.btVector3(0.5, 6, 22.5);
 
                 return {
                     mass: 0,
                     margin: 0,
-                    pos: new Ammo.btVector3(0, 3, -11),
+                    pos: new Ammo.btVector3(-19.5, 6, 2.5),
                     // rot: {x: 0, y: 45, z: 0},
                     threeObject: (function () {
                         return new THREE.Mesh(
@@ -387,17 +431,84 @@ class APP {
                                 size.z() * 2
                             ),
                             new THREE.MeshPhongMaterial({
-                                color: 0xcccccc,
-                                specular: 0x222222
+                                color: 0x464749,
+                                emissive: 0x000000,
+                                specular: 0x555555,
+                                shininess: 150
                             })
                         );
                     }()),
                     collisionShape: new Ammo.btBoxShape(size),
                     localInertia: new Ammo.btVector3(0, 0, 0),
                     afterPhy: (body) => {
-                        // return;
-                        body.setRestitution(10); // 反発係数
-                        body.setFriction(10); // 摩擦係数
+                        body.setRestitution(1); // 反発係数
+                        body.setFriction(500); // 摩擦係数
+                        body.setAngularFactor(new Ammo.btVector3(0, 0, 0)); // 回転制限
+                        body.setLinearFactor(new Ammo.btVector3(0, 0, 0)); // 滑り制限
+                    }
+                };
+            },
+            wallRight: function () {
+                let size = new Ammo.btVector3(0.5, 6, 22.5);
+
+                return {
+                    mass: 0,
+                    margin: 0,
+                    pos: new Ammo.btVector3(19.5, 6, 2.5),
+                    // rot: {x: 0, y: 45, z: 0},
+                    threeObject: (function () {
+                        return new THREE.Mesh(
+                            new THREE.BoxGeometry(
+                                size.x() * 2,
+                                size.y() * 2,
+                                size.z() * 2
+                            ),
+                            new THREE.MeshPhongMaterial({
+                                color: 0x464749,
+                                emissive: 0x000000,
+                                specular: 0x555555,
+                                shininess: 150
+                            })
+                        );
+                    }()),
+                    collisionShape: new Ammo.btBoxShape(size),
+                    localInertia: new Ammo.btVector3(0, 0, 0),
+                    afterPhy: (body) => {
+                        body.setRestitution(1); // 反発係数
+                        body.setFriction(500); // 摩擦係数
+                        body.setAngularFactor(new Ammo.btVector3(0, 0, 0)); // 回転制限
+                        body.setLinearFactor(new Ammo.btVector3(0, 0, 0)); // 滑り制限
+                    }
+                };
+            },
+            wallBack: function () {
+                let size = new Ammo.btVector3(20, 6, 0.5);
+
+                return {
+                    mass: 0,
+                    margin: 0,
+                    pos: new Ammo.btVector3(0, 6, -20.5),
+                    // rot: {x: 0, y: 45, z: 0},
+                    threeObject: (function () {
+                        return new THREE.Mesh(
+                            new THREE.BoxGeometry(
+                                size.x() * 2,
+                                size.y() * 2,
+                                size.z() * 2
+                            ),
+                            new THREE.MeshPhongMaterial({
+                                color: 0x464749,
+                                emissive: 0x000000,
+                                specular: 0x555555,
+                                shininess: 150
+                            })
+                        );
+                    }()),
+                    collisionShape: new Ammo.btBoxShape(size),
+                    localInertia: new Ammo.btVector3(0, 0, 0),
+                    afterPhy: (body) => {
+                        body.setRestitution(1); // 反発係数
+                        body.setFriction(500); // 摩擦係数
                         body.setAngularFactor(new Ammo.btVector3(0, 0, 0)); // 回転制限
                         body.setLinearFactor(new Ammo.btVector3(0, 0, 0)); // 滑り制限
                     }
@@ -405,67 +516,59 @@ class APP {
             },
             medals: function () {
                 let radius = 3;
-                let size = new Ammo.btVector3(radius, 0.5, radius);
+                let height = 0.8;
+                let size = new Ammo.btVector3(radius, height / 2, radius);
+                let face = this.phongMaterial();
+                let side = this.phongMaterial();
+
+                side.map = this.texture.medalSide;
+                side.bumpMap = this.texture.medalSideBump;
+                face.bumpMap = this.texture.medalFaceBump;
 
                 return {
-                    mass: 1,
-                    margin: 0.05,
-                    pos: new Ammo.btVector3(0, 25, -5),
-                    threeObject: (function () {
+                    mass: 100,
+                    margin: 0.1,
+                    pos: new Ammo.btVector3(0, 25, -15),
+                    threeObject: (() => {
                         return new THREE.Mesh(
                             new THREE.CylinderGeometry(
                                 radius,
                                 radius,
-                                1,
+                                height,
                                 24
                             ),
-                            new THREE.MeshPhongMaterial({
-                                color: Math.floor(Math.random() * (1 << 24)),
-                                specular: 0xcccccc
-                            })
+                            [
+                                side,
+                                face,
+                                face
+                            ]
                         );
-                    }()),
+                    })(),
                     collisionShape: new Ammo.btCylinderShape(size),
                     localInertia: new Ammo.btVector3(0, 0, 0),
                     afterPhy: function (body) {
-                        // // Z軸に加速度(1)を追加
+                        // 加速度
                         body.setLinearVelocity(new Ammo.btVector3(
-                            Math.floor(Math.random() * (21 - -20)) + -20, -10, -5));
-                        // // 反発係数を設定
-                        // body.setRestitution(0);
-                        // // 摩擦係数を設定
-                        // body.setFriction(2);
-                        // // 減衰率を設定
-                        // body.setDamping(0, 0.1);
-                        // // 回転制限（1の軸でしか回転しない）
-                        body.setAngularFactor(new Ammo.btVector3(1, 1, 1));
-                        // // 滑り制限（1の軸でしか動かない。初期値には適用されない）
-                        body.setLinearFactor(new Ammo.btVector3(1, 1, 1));
+                            Math.floor(Math.random() * (21 - -20)) + -20, -10, 0)
+                        );
+                        body.setRestitution(0.05); // 反発係数
+                        // body.setFriction(50);         // 摩擦係数
+                        body.setFriction(0.2);         // 摩擦係数
+                        body.setDamping(0, 0.1);      // 減衰率
+                        body.setAngularFactor(new Ammo.btVector3(1, 1, 1)); // 回転制限
+                        body.setLinearFactor(new Ammo.btVector3(1, 1, 1));  // 滑り制限
                     },
                     updatePhy: function (body, p, q, objThree, i) {
                         if (p.y() <= -10) {
-                            // Ammo.destroy(body);
-                            // console.log('destroy')
-                            // console.log(this)
-                            // body.__destroy__();
-                            // console.log(body.getMotionState())
-                            // console.log(body.getCollisionShape())
-                            // Ammo.destroy(body.getMotionState());
                             this.physicsWorld.removeRigidBody(body);
                             this.dynamicObjects[i] = {
                                 userData: false
                             };
                             this.scene.remove(objThree);
-                            objThree.material.dispose();
+                            objThree.material[0].dispose();
+                            objThree.material[1].dispose();
+                            objThree.material[2].dispose();
                             objThree.geometry.dispose();
-
-
-                            // this.dynamicObjects[i] = {
-                            //     userData: false
-                            // };
-                            // this.scene.remove(objThree);
-                            // objThree.material.dispose();
-                            // objThree.geometry.dispose();
                         }
                     }.bind(this)
                 };
