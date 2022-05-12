8d4b5e9073b47feb1c09463a319809720e5a5add
izmnull <izm.library@gmail.com>
2022-05-12 18:57:56 +0900

    chk

--- run.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var exports = {}
-exports.__esModule = true;
-var THREE = require("three");
-// Comment
-var scene = new THREE.Scene();
--- run.ts
+++
@@ -1,3 +0,0 @@
-import * as THREE from 'three';
-// Comment
-const scene = new THREE.Scene();
